7b6bfcc275c2f9e8dedb195a2c4110537563c5c6
Hotel Finder <petjamm@tut.ac.za>
2022-02-28 12:18:29 +0200

    latest

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState,useEffect} from 'react';
+import Start from './component/Start';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './component/Login';
+import Register from './component/Register';
+import Test from './component/Test';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import firebase from 'firebase';
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+    return (
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="start"
+          component={Start}
+          options={{header: () => null}}
+          
+        />
+        <Stack.Screen name="Login" component={Login} 
+           options={{header: () => null}} />
+            <Stack.Screen name="Register" component={Register} 
+           options={{header: () => null}} />
+            <Stack.Screen name="Test" component={Test} 
+           options={{header: () => null}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
+  
+  
+};
---
+++ component/Home.js
@@ -0,0 +1,75 @@
+import { StatusBar } from 'expo-status-bar';
+import {useNavigation} from '@react-navigation/native'
+import { Pressable,ImageBackground,TouchableOpacity,Image,StyleSheet,TextInput, Button, View,Text, Alert } from 'react-native';
+import firebase from 'firebase';
+
+export default function Home() {
+  const navigation = useNavigation();
+  const Logout = () => {
+    navigation.navigate('Login')
+    firebase.auth().signOut();
+  
+ 
+  }
+   
+
+  return (
+    <View style={styles.container}>
+ <View style ={styles.TopView}>
+  
+ </View> 
+ <View style ={styles.ButtomView}>
+ <TouchableOpacity onPress={Logout}  style={styles.Button} >
+     <Text style={styles.ButtonText}>Logout</Text>
+  </TouchableOpacity>
+ </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center'
+  
+  },
+  TopView: {
+    width:'100%',
+    height:'40%',
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  ButtomView:{
+    width:'100%',
+    height:'60%',
+    display:'flex',
+justifyContent:'flex-end',
+alignItems:'center'
+  },
+  logo:{
+    width:'70%',
+    resizeMode:'contain',
+  },
+  Button:{
+width:'90%',
+color:'#000',
+height:50,
+borderRadius:5,
+backgroundColor:'#378CE8',
+display:'flex',
+margin:20,
+justifyContent:'center',
+alignItems:'center'
+
+  },
+  ButtonText:{
+    color:"#fff",
+    fontWeight:'bold',
+    fontSize:16  }
+});
